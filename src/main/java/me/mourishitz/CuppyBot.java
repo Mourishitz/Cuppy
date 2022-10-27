b916aa932c323c49b8033897552e623d2a9ea3e9
Mourishitz <gabrielmorishita@hotmail.com>
2022-10-27 00:43:15 -0300

    🔐 Token escondido em .env

--- src/main/java/me/mourishitz/CuppyBot.java
+++ src/main/java/me/mourishitz/CuppyBot.java
@@ -1,5 +1,6 @@
 package me.mourishitz;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -10,14 +11,20 @@ import javax.security.auth.login.LoginException;
 
 public class CuppyBot {
 
+    private final Dotenv config;
     private final ShardManager shardManager;
 
     public CuppyBot() throws LoginException {
-        String token = "sai daqui maluco";
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("☕"));
         shardManager = builder.build();
+    }
+
+    public Dotenv getConfig(){
+        return config;
     }
 
     public ShardManager getShardManager() { return shardManager; }
